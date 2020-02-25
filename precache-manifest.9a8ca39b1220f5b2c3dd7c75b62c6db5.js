a361b73db0a127289129ae7c6b63fd082a1022a6
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2020-02-25 21:08:19 +0100

    Updates

---
+++ precache-manifest.9a8ca39b1220f5b2c3dd7c75b62c6db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728b15a6071017c5eea0004370233a8b",
+    "url": "/my-app6/index.html"
+  },
+  {
+    "revision": "2fc302e29482c69fb752",
+    "url": "/my-app6/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8921723c37a113570520",
+    "url": "/my-app6/static/js/2.29479bfe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app6/static/js/2.29479bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc302e29482c69fb752",
+    "url": "/my-app6/static/js/main.4ad43ad5.chunk.js"
+  },
+  {
+    "revision": "710c2e1a393adb95ea3e",
+    "url": "/my-app6/static/js/runtime-main.5f694f72.js"
+  }
+]);
